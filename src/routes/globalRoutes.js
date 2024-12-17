0353313bbe2526c2a683413ba74396760fb77b70
MiguelAngel117 <miguel.choconta@uptc.edu.co>
2024-12-17 16:28:11 -0500

    Update GlobalRoutes Of API

--- src/routes/globalRoutes.js
+++ src/routes/globalRoutes.js
@@ -3,7 +3,11 @@ const router = express.Router();
 
 const api = '/turnity'; 
 
-router.use(`${api}/shifts`, require('./api/shifts'));
 router.use(`${api}/employees`, require('./api/employees'));
+router.use(`${api}/stores`, require('./api/stores'));
+router.use(`${api}/departments`, require('./api/departments'));
+router.use(`${api}/departmentStore`, require('./api/departmentStore'));
+router.use(`${api}/position`, require('./api/positions'));
+router.use(`${api}/shifts`, require('./api/shifts'));
 
 module.exports = router;
